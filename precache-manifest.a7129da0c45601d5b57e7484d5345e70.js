ca5338cb61474a22b20a7a12a60ebe8a3b121a4d
Koke-1 <sanjuangeorge@gmail.com>
2020-12-22 11:40:34 -0600

    Updates

---
+++ precache-manifest.a7129da0c45601d5b57e7484d5345e70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "152992ea677dcafc6efc2486aa8680dd",
+    "url": "/Shoplist/index.html"
+  },
+  {
+    "revision": "e54f660e7f50061d4ddf",
+    "url": "/Shoplist/static/css/main.07b1b8e2.chunk.css"
+  },
+  {
+    "revision": "5fc7a745176ee1c7ee3d",
+    "url": "/Shoplist/static/js/2.31eded13.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Shoplist/static/js/2.31eded13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54f660e7f50061d4ddf",
+    "url": "/Shoplist/static/js/main.048f8aac.chunk.js"
+  },
+  {
+    "revision": "cff6fe86d2da97177b76",
+    "url": "/Shoplist/static/js/runtime-main.bda4c2d2.js"
+  }
+]);
